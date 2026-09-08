8b72d8c744e6cab097965f38103c8faa54a69eb0
nileshdubey404 <nileshdubey404@gmail.com>
2026-09-08 18:13:04 +0530

    creating targer group resource for alb

--- main.tf
+++ main.tf
@@ -25,3 +25,12 @@ module "jenkins" {
   enable_public_ip_address  = true
  user_data_install_jenkins = templatefile("./jenkins-runner-script/jenkins-installer.sh", {})
 }
+
+module "lb_target_group" {
+  source                   = "./load-balancer-target-group"
+  lb_target_group_name     = "jenkins-lb-target-group"
+  lb_target_group_port     = 8080
+  lb_target_group_protocol = "HTTP"
+  vpc_id                   = module.networking.aws_infra_vpc_id
+  ec2_instance_id          = module.jenkins.jenkins_ec2_instance_id
+}
